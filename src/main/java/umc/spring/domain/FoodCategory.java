75b65f478f1c568fe784f272dbe25294017acb9e
YeomSB <stanley0927@naver.com>
2023-11-17 19:25:54 +0900

    17/19:25

---
+++ src/main/java/umc/spring/domain/FoodCategory.java
@@ -0,0 +1,20 @@
+package umc.spring.domain;
+
+import lombok.*;
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class FoodCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+
+}
