e329f69d9e9758085b0433ae903cc36895e61ec0
YeomSB <stanley0927@naver.com>
2023-12-03 16:59:54 +0900

    feat: 미션수행(수정)

--- src/main/java/umc/spring/web/controller/MissionRestController.java
+++ src/main/java/umc/spring/web/controller/MissionRestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import umc.spring.apiPayload.ApiResponse;
 import umc.spring.converter.MissionConverter;
 import umc.spring.domain.Mission;
-import umc.spring.service.MissionSevice.MissionCommandService;
+import umc.spring.service.MissionService.MissionCommandService;
 import umc.spring.web.dto.MissionRequestDTO;
 import umc.spring.web.dto.MissionResponseDTO;
 
